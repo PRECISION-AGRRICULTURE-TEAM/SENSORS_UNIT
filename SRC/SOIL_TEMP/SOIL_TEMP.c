6bec15b2bc6ddb77cac9089a6ad3c41b353d2359
Ahmed Rashad <ahmedmrashad96@gmail.com>
2020-05-16 15:36:51 +0200

    added the prototypes of the driver

---
+++ SRC/SOIL_TEMP/SOIL_TEMP.c
@@ -0,0 +1,7 @@
+/** @file SOIL_TEMP.c
+ *  @brief This file contains the Source Code of the SOIL_TEMP Driver
+ *  @author  
+ */
+
+#include "SOIL_TEMP_LOCAL.h"
+
---
+++ SRC/SOIL_TEMP/SOIL_TEMP_CFG.h
@@ -0,0 +1,9 @@
+/** @file SOIL_TEMP_CFG.h
+ *  @brief This file contains the Constants of the SOIL_TEMP Driver
+ *  @author  
+ */
+#ifndef SOIL_TEMP_CFG_H_
+#define SOIL_TEMP_CFG_H_
+
+
+#endif /* SOIL_TEMP_H_ */
